4f2e104d92624278d650568e6675cb05fdc4850f
Mathias Salva <mattsalva@outlook.com>
2023-03-06 04:39:05 -0300

    Agregada logica de filtros de busqueda

--- scripts/home.js
+++ scripts/home.js
@@ -1,29 +1,124 @@
+const catElement = document.getElementById('search-filter')
 const dataElement = document.getElementById('eventos')
+const rawEventsData = data.events
 
-
-/*Información parcial que se muestra en la tarjeta. El resto se visualizara en "Details"*/
-for (let i = 0; i < data.events.length; i++){
-    containerCart = document.createElement('div');
-    containerCart.setAttribute("class", "col-lg-4 d-flex align-items-stretch")
-    containerCart.innerHTML = `
+/*Agrega eventos en forma de card
+* dataNode: Nodo padre de los eventos
+* data: Array de eventos*/
+function addCards(dataNode, data){
+    let containerCart;
+    dataNode.replaceChildren()
+    for (let i = 0; i < data.length; i++) {
+        containerCart = document.createElement('div');
+        containerCart.setAttribute("class", "col-lg-4 d-flex align-items-stretch")
+        containerCart.innerHTML = `
                 <div class="card">
-                    <img src=${data.events[i].image} class="card-img-top" alt="">
+                    <img src=${data[i].image} class="card-img-top" alt="">
                     <div class="card-body">
-                        <h5 class="card-title ">${data.events[i].name}</h5>
-                        <p class="card-text text-black mb-0">${data.events[i].date}</p>
-                        <p class="card-text text-success mb-0">${data.events[i].category}</p>
-                        <p class="card-text ">${data.events[i].description}</p>
+                        <h5 class="card-title ">${data[i].name}</h5>
+                        <p class="card-text text-black mb-0">${data[i].date}</p>
+                        <p class="card-text text-success mb-0">${data[i].category}</p>
+                        <p class="card-text ">${data[i].description}</p>
                         <div class="row">
                             <div class="col">
-                                <span class="fs-5">Price $${data.events[i].price}</span>
+                                <span class="fs-5">Price $${data[i].price}</span>
                             </div>
                             <div class="col">
-                                <a href="details.html" class="btn btn-secondary">Ver mas...</a>
+                                <a href="details.html/?id=${data[i]._id}" class="btn btn-secondary">Ver mas...</a>
                             </div>
                         </div>
 
                     </div>
                 </div>
             `
-    dataElement.appendChild(containerCart)
+        dataNode.appendChild(containerCart)
+    }
+}
+
+/*
+* Listar categorias dinamicamente a partir de los eventos disponibles
+* catNode: Nodo parent de las categorias
+* data: Data de los eventos
+* */
+function listCategories(catNode, data){
+    let containerCats;
+    let categories = data.map(x => x.category)
+    categories = [...new Set(categories)]
+    const formNode = document.getElementById("search-box")
+    for (let i = 0; i < categories.length; i++){
+        let category = categories[i].replace(" ","-").toLowerCase()
+        containerCats = document.createElement('div')
+        containerCats.innerHTML = `
+                    <input type="checkbox" name="category" id="${category}" value="${category}">
+                    <label for="${category}">${categories[i]}</label>`
+
+        catNode.insertBefore(containerCats, formNode)
+    }
+}
+/*Normaliza texto
+* text: string texto */
+
+function normalizeText(text){
+    return text.trim().toLowerCase()
+}
+/*Normaliza categoria pasando a minuscula y reemplazando espacios por "-"
+* category: string categoria a normalizar*/
+function normalizeCategory(category){
+    return category.replace(" ","-").toLowerCase()
+}
+
+/*Filtrar eventos a partir de input de busqueda por casillas y por texto
+* rawEventsData = array de eventos sin filtrar
+* */
+function filterEvents(rawEventsData){
+    const querystring = location.search
+    const params = new URLSearchParams(querystring);
+    let q = params.get('q')
+    q = normalizeText(q)
+    let catQuery = params.getAll('category')
+    let filteredEventsData = []
+    if (catQuery.length && q.length){
+        filteredEventsData = rawEventsData.filter(e => (
+            (catQuery.includes(normalizeCategory(e.category)))
+            &&
+            (normalizeText(e.name).includes(q) || normalizeText(e.description).includes(q))
+        ))
+        if (filteredEventsData.length === 0)
+            alert("No se encontraron resultados")
+        else
+            addCards(dataElement, filteredEventsData)
+    }
+    else if (catQuery.length && !q.length){
+        filteredEventsData = rawEventsData.filter(e => (
+            catQuery.includes(normalizeCategory(e.category))))
+
+        if (filteredEventsData.length === 0)
+            alert("No se encontraron resultados")
+        else
+            addCards(dataElement, filteredEventsData)
+    }
+    else if (!catQuery.length && q.length){
+        filteredEventsData = rawEventsData.filter(e => (
+            normalizeText(e.name).includes(q) || normalizeText(e.description).includes(q)))
+
+        console.log(filteredEventsData)
+
+        if (filteredEventsData.length === 0)
+            alert("No se encontraron resultados")
+         else
+            addCards(dataElement, filteredEventsData)
+
+    }
+
+
+}
+
+
+addCards(dataElement, rawEventsData)
+listCategories(catElement, rawEventsData)
+
+
+
+if (location.search){
+    filterEvents(rawEventsData)
 }
--- scripts/past_events.js
+++ scripts/past_events.js
@@ -1,3 +1,5 @@
+const catElement = document.getElementById('search-filter')
+const dataElement = document.getElementById('eventos')
 let current = new Date(data.currentDate)
 let pastDates
 data.events.forEach((el) => {
@@ -5,31 +7,108 @@ data.events.forEach((el) => {
 })
 
 
-const dataElement = document.getElementById('eventos')
-
-
-for (let i = 0; i < pastDates.length; i++){
-    containerCart = document.createElement('div');
-    containerCart.setAttribute("class", "col-lg-4 d-flex align-items-stretch")
-    containerCart.innerHTML = `
+/*Agrega eventos en forma de card
+* dataNode: Nodo padre de los eventos
+* data: Array de eventos*/
+function addCards(dataNode, data){
+    let containerCart;
+    dataNode.replaceChildren()
+    for (let i = 0; i < data.length; i++) {
+        containerCart = document.createElement('div');
+        containerCart.setAttribute("class", "col-lg-4 d-flex align-items-stretch")
+        containerCart.innerHTML = `
                 <div class="card">
-                    <img src=${pastDates[i].image} class="card-img-top" alt="">
+                    <img src=${data[i].image} class="card-img-top" alt="">
                     <div class="card-body">
-                        <h5 class="card-title ">${pastDates[i].name}</h5>
-                        <p class="card-text text-black mb-0">${pastDates[i].date}</p>
-                        <p class="card-text text-success mb-0">${pastDates[i].category}</p>
-                        <p class="card-text ">${pastDates[i].description}</p>
+                        <h5 class="card-title ">${data[i].name}</h5>
+                        <p class="card-text text-black mb-0">${data[i].date}</p>
+                        <p class="card-text text-success mb-0">${data[i].category}</p>
+                        <p class="card-text ">${data[i].description}</p>
                         <div class="row">
                             <div class="col">
-                                <span class="fs-5">Price $${pastDates[i].price}</span>
+                                <span class="fs-5">Price $${data[i].price}</span>
                             </div>
                             <div class="col">
-                                <a href="details.html" class="btn btn-secondary">Ver mas...</a>
+                                <a href="details.html/?id=${data[i]._id}" class="btn btn-secondary">Ver mas...</a>
                             </div>
                         </div>
 
                     </div>
                 </div>
             `
-    dataElement.appendChild(containerCart)
+        dataNode.appendChild(containerCart)
+    }
+}
+
+/*
+* Listar categorias dinamicamente a partir de los eventos disponibles
+* catNode: Nodo parent de las categorias
+* data: Data de los eventos
+* */
+function listCategories(catNode, data){
+    let containerCats;
+    let categories = data.map(x => x.category)
+    categories = [...new Set(categories)]
+    const formNode = document.getElementById("search-box")
+    for (let i = 0; i < categories.length; i++){
+        let category = categories[i].replace(" ","-").toLowerCase()
+        containerCats = document.createElement('div')
+        containerCats.innerHTML = `
+                    <input type="checkbox" name="category" id="${category}" value="${category}">
+                    <label for="${category}">${categories[i]}</label>`
+
+        catNode.insertBefore(containerCats, formNode)
+    }
+}
+/*Normaliza texto
+* text: string texto */
+
+function normalizeText(text){
+    return text.trim().toLowerCase()
+}
+/*Normaliza categoria pasando a minuscula y reemplazando espacios por "-"
+* category: string categoria a normalizar*/
+function normalizeCategory(category){
+    return category.replace(" ","-").toLowerCase()
+}
+
+/*Filtrar eventos a partir de input de busqueda por casillas y por texto
+* rawEventsData = array de eventos sin filtrar
+* */
+function filterEvents(rawEventsData){
+    const querystring = location.search
+    const params = new URLSearchParams(querystring);
+    let q = params.get('q')
+    q = normalizeText(q)
+    let catQuery = params.getAll('category')
+    let filteredEventsData = []
+    if (catQuery.length && q.length){
+        filteredEventsData = rawEventsData.filter(e => (
+            (catQuery.includes(normalizeCategory(e.category)))
+            &&
+            (normalizeText(e.name).includes(q) || normalizeText(e.description).includes(q))
+        ))
+        addCards(dataElement, filteredEventsData)
+    }
+    else if (catQuery.length && !q.length){
+        filteredEventsData = rawEventsData.filter(e => (
+            catQuery.includes(normalizeCategory(e.category))))
+
+        addCards(dataElement, filteredEventsData)
+    }
+    else if (!catQuery.length && q.length){
+        filteredEventsData = rawEventsData.filter(e => (
+            normalizeText(e.name).includes(q) || normalizeText(e.description).includes(q)))
+        addCards(dataElement, filteredEventsData)
+    }
+}
+
+
+addCards(dataElement, pastDates)
+listCategories(catElement, pastDates)
+
+
+
+if (location.search){
+    filterEvents(pastDates)
 }
